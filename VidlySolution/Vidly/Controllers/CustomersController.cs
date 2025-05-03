ea1b86b83830d44234ba387f3772791fedd3eb88
Shashwat Singh <shashwatsingh1@arizona.edu>
2025-05-02 19:28:17 -0700

    Added Validation For Customer Name

--- VidlySolution/Vidly/Controllers/CustomersController.cs
+++ VidlySolution/Vidly/Controllers/CustomersController.cs
@@ -53,6 +53,15 @@ namespace Vidly.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if(!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel()
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes
+                };
+                return View("CustomerForm", viewModel);
+            }
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
